a4c9c07f71626aa951ca953a0d3f4050f36bc5b4
Garen Hart <garenhart@gmail.com>
2022-03-15 10:15:57 -0400

    Dynamic live_loop management library

---
+++ lib/lib-dyn-live_loop.rb
@@ -0,0 +1,19 @@
+# Library inspired by:
+# https://in-thread.sonic-pi.net/t/programmatically-creating-live-loops-on-the-fly/1919/10
+
+RUNSTATE_KEY = "ll_runstate_"
+LIVE_LOOP_NAME_KEY = "ll_"
+
+define :runLoop do |name, fn|
+  loopsym = (RUNSTATE_KEY + name).to_sym
+  set loopsym, true
+  live_loop (LIVE_LOOP_NAME_KEY + name).to_sym do
+    fn.call()
+    stop if ! get(loopsym)
+  end
+end
+
+define :stopLoop do |name|
+  set (RUNSTATE_KEY + name).to_sym, false
+end
+
