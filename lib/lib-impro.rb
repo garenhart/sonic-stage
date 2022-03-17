18d26a7de7d2e98ec76b36ec793497d0fd5a94e8
Garen Hart <garenhart@gmail.com>
2022-03-17 17:45:40 -0400

    Add bass amp and move play_bass to library

--- lib/lib-impro.rb
+++ lib/lib-impro.rb
@@ -7,6 +7,16 @@ define :li_play_drum do |drum_sample, beats, amp, on=true|
   end
 end
 
+define :li_play_bass do |mode, tonics, tonics_pos, amp|
+  16.times do |i|
+    if (tonics.size > 0) && (mode == 0)
+      play tonics.tick, amp: amp
+    end
+    sleep 0.25
+  end
+end
+
+
 define :li_root_sequence do |root, dur|
   16.times do
     play root
