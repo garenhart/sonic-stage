cc0adad7ec0aefdc54bb4842f5f96c97b39db9e0
Garen Hart <garenhart@gmail.com>
2022-04-11 17:44:31 -0400

    Chord types and pattern support

--- lib/lib-impro.rb
+++ lib/lib-impro.rb
@@ -26,23 +26,57 @@ define :li_play_bass do |tonics, tonics_pos, amp|
   end
 end
 
-define :li_play_chords do |tonics, tonics_pos, amp, mode, scale, chord_type|
+define :li_play_chords do |tonics, tonics_pos, amp, mode, scale, pattern, chord_type|
   if (tonics_pos.size > 0) && (tonics_pos.size == tonics.size)
     m_scale = mode_scale mode, scale
     puts "SCALE", m_scale
 
+    seq = 1
+    case pattern
+    when 1
+      seq = [1]
+    when 2
+      seq = [1] # update later
+    when 3
+      seq = [1] # update later
+    when 4
+      seq = [2,5,1]
+    end
+
+    seven = false
+    rootless = false
+    tonic = false
+    case chord_type
+    when 1
+      tonic = true
+    when 3
+      rootless = true
+    when 4
+      seven = true
+    when 5
+      seven = true
+      rootless = true
+    end
+
+
+
     cs = []
-    chord_num = 0
+    last_pos = -1
+    last_ind = 0
     16.times do |i|
       pos = tonics_pos.index(i)
       if (pos)
-        chord_num = 2
-        cs = chord_seq(tonics[pos], m_scale, [2,5,1])
+        last_pos = pos
+        last_ind = i
+        cs = chord_seq(tonics[pos], m_scale, seq, seven, rootless)
         puts "chords", cs
-        play cs[chord_num], amp: amp
-#      else
-#        chord_num = chord_num + 1
-#        play cs[chord_num], amp: amp # if chord_num < cs.length && chord_num > 0
+        play (tonic ? cs[0][0] : cs[0]), amp: amp
+      else
+        chord_num = i-last_ind
+        if (chord_num < cs.length) && (i < 16)
+          puts "III", i
+          play cs[chord_num], amp: amp
+        end
       end
       sleep 0.25
     end
