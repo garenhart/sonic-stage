6231b9315eb3ae33e45fa27ad92c631e6e93c2bd
Garen Hart <garenhart@gmail.com>
2022-12-07 17:00:18 -0500

    Parametric Serialization / Deserialization Fixes #7

--- lib-io.rb
+++ lib-io.rb
@@ -1,14 +1,26 @@
 # configuration folder path
-configFilePath = get(:sp_path) + "live-impro\\sonic-pi-open-stage-control\\config\\" #path for config files
+# configPath = get(:sp_path) + "live-impro\\sonic-pi-open-stage-control\\config\\" #path for config files
 
 # read from JSON file and return parsed hash
-define :gl_readJSON do |file_name, default_path = true|
-    file = File.read((default_path ? configFilePath : "") + file_name) 
+define :gl_readJSON do |file_name|
+    file = File.read(file_name) 
     return JSON.parse(file)
     # return  MultiJson.load(file) # this works as well
 end
 
+# insert suffix into file name before extension
+define :gl_suffix_filename do |file_name, suffix|
+    extension = File.extname(file_name)
+    file_name.reverse.sub(extension.reverse, ("#{suffix}#{extension}").reverse).reverse
+end
+
 # save hash into JSON file
 define :gl_writeJSON do |file_name, hash|
-    File.write(configFilePath + file_name +".json", JSON.dump(hash)) 
+    File.write(file_name, JSON.dump(hash)) 
+end
+
+# create unique name based on current date-time and save hash into it
+define :gl_write_unique_JSON do |file_name, hash|
+    file_name_new = gl_suffix_filename(file_name, DateTime.now.strftime("_%m-%d-%y-%k%M%S-%L"))
+    File.write(file_name_new, JSON.dump(hash)) 
 end
