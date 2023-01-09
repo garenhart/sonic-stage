1aa72bb9165ea0d0ef7553ccec2a9a7e655bfb2b
Garen Hart <garenhart@gmail.com>
2023-01-09 17:20:13 -0500

    Get rid of method prefix across all libraries.

--- lib/lib-init.rb
+++ lib/lib-init.rb
@@ -1,13 +1,11 @@
 #######################
 # lib-init.rb
 # initialization/configuration library
-# gl_ prefix is used for methods to indicate "garen's library"
-#     in absence of support for namespaces and classes 
 # author: Garen H.
 #######################
 
 # get the sample group name from a sample name (first substring before "_")
-define :gl_sample_group do |s|
+define :sample_group do |s|
   s.split("_").first
 end
 
@@ -28,52 +26,52 @@ define :init_tonics do |cfg|
   cfg['chords']['pattern'] = []
 end
 
-define :gl_init_keyboard do |tonic, mode|
+define :init_keyboard do |tonic, mode|
   scale_notes = scale tonic, mode
   for note in 21..107
-    gl_osc_ctrl "/keyboard_scale", note, (scale_notes.to_a.include? note) ? 1 : 0
+    osc_ctrl "/keyboard_scale", note, (scale_notes.to_a.include? note) ? 1 : 0
   end
 end
 
 define :init_drum do |d, gr_ctrl, inst_ctrl, cfg|
-  sample_gr = gl_sample_group(cfg[d]['sample'])
-  gl_osc_ctrl "/#{d}", cfg[d]['on'] ? 1 : 0
-  gl_osc_ctrl "/#{d}_amp", cfg[d]['amp']
-  gl_osc_ctrl gr_ctrl, sample_gr
-  gl_populate_samples inst_ctrl + "_v", sample_gr.to_sym
+  sample_gr = sample_group(cfg[d]['sample'])
+  osc_ctrl "/#{d}", cfg[d]['on'] ? 1 : 0
+  osc_ctrl "/#{d}_amp", cfg[d]['amp']
+  osc_ctrl gr_ctrl, sample_gr
+  populate_samples inst_ctrl + "_v", sample_gr.to_sym
   sleep 0.125 # sleeping between populating and selecting seems to make it work 
-  gl_osc_ctrl inst_ctrl, (cfg[d]['sample'])
+  osc_ctrl inst_ctrl, (cfg[d]['sample'])
   16.times do |i|
-    gl_osc_ctrl "/#{d}_beats/#{i}", cfg[d]['beats'][i] #should figure out how to populate beats without looping through array
+    osc_ctrl "/#{d}_beats/#{i}", cfg[d]['beats'][i] #should figure out how to populate beats without looping through array
   end
-  # gl_osc_ctrl "/#{d}_beats", cfg[d]['beats']
+  # osc_ctrl "/#{d}_beats", cfg[d]['beats']
   # set (d.to_sym), cfg[d]['beats'] #set the beats to for the drum
 end
 
 define :init_drums do |cfg|
-  gl_osc_ctrl "/drums", 1
-  gl_osc_ctrl "/dropdown_drum_tempo_factor", cfg['drum_tempo_factor']
+  osc_ctrl "/drums", 1
+  osc_ctrl "/dropdown_drum_tempo_factor", cfg['drum_tempo_factor']
   init_drum "kick", "/kick_inst_groups", "/kick_inst", cfg
   init_drum "snare", "/snare_inst_groups", "/snare_inst", cfg
   init_drum "cymbal", "/cymbal_inst_groups", "/cymbal_inst", cfg
 end
 
 define :init_controls do |cfg|
-  gl_osc_ctrl "/tempo", cfg['tempo']
-  gl_osc_ctrl "/pattern_mode", cfg['pattern_mode']
-  gl_osc_ctrl "/pattern", cfg['pattern']
-  gl_osc_ctrl "/switch_loop", cfg['loop_mode']
-  gl_osc_ctrl "/mode", cfg['mode']
-  gl_osc_ctrl "/scale", cfg['scale']
-  gl_osc_ctrl "/bass_inst", cfg['bass']['synth']
-  gl_osc_ctrl "/bass_amp", cfg['bass']['amp']
-  gl_osc_ctrl "/chord_type", cfg['chords']['type']
-  gl_osc_ctrl "/chord_inst", cfg['chords']['synth']
-  gl_osc_ctrl "/chord_amp", cfg['chords']['amp']
+  osc_ctrl "/tempo", cfg['tempo']
+  osc_ctrl "/pattern_mode", cfg['pattern_mode']
+  osc_ctrl "/pattern", cfg['pattern']
+  osc_ctrl "/switch_loop", cfg['loop_mode']
+  osc_ctrl "/mode", cfg['mode']
+  osc_ctrl "/scale", cfg['scale']
+  osc_ctrl "/bass_inst", cfg['bass']['synth']
+  osc_ctrl "/bass_amp", cfg['bass']['amp']
+  osc_ctrl "/chord_type", cfg['chords']['type']
+  osc_ctrl "/chord_inst", cfg['chords']['synth']
+  osc_ctrl "/chord_amp", cfg['chords']['amp']
   
-  gl_osc_ctrl "/bass_points", cfg['tonics'].length
-  gl_osc_ctrl "/chord_points", cfg['tonics'].length
-  gl_init_keyboard(cfg['tonics'][0], cfg['scale'])
+  osc_ctrl "/bass_points", cfg['tonics'].length
+  osc_ctrl "/chord_points", cfg['tonics'].length
+  init_keyboard(cfg['tonics'][0], cfg['scale'])
 
   init_drums cfg
 end
--- lib/lib-io.rb
+++ lib/lib-io.rb
@@ -1,27 +1,33 @@
+#######################
+# lib-io.rb
+# input/output library
+# author: Garen H.
+#######################
+
 # configuration folder path
 # configPath = get(:sp_path) + "live-impro\\sonic-pi-open-stage-control\\config\\" #path for config files
 
 # read from JSON file and return parsed hash
-define :gl_readJSON do |file_name|
+define :readJSON do |file_name|
     file = File.read(file_name) 
     return JSON.parse(file)
     # return  MultiJson.load(file) # this works as well
 end
 
 # insert suffix into file name before extension
-define :gl_suffix_filename do |file_name, suffix|
+define :suffix_filename do |file_name, suffix|
     extension = File.extname(file_name)
     file_name.reverse.sub(extension.reverse, ("#{suffix}#{extension}").reverse).reverse
 end
 
 # save hash into JSON file
-define :gl_writeJSON do |file_name, hash|
+define :writeJSON do |file_name, hash|
     File.write(file_name, JSON.pretty_generate(hash, indent: "    ")) 
 end
 
 # create unique name based on current date-time and save hash into it
-define :gl_write_unique_JSON do |file_name, hash|
-    file_name_new = gl_suffix_filename(file_name, DateTime.now.strftime("_%m-%d-%y-%k%M%S-%L"))
+define :write_unique_JSON do |file_name, hash|
+    file_name_new = suffix_filename(file_name, DateTime.now.strftime("_%m-%d-%y-%k%M%S-%L"))
     # File.write(file_name_new, JSON.pretty_generate(hash, indent: "    "))
     File.write(file_name_new, JSON.pretty_generate(hash, array_nl: "")) # array_nl: "" puts array in one string, but still retains appropriate indentation which makes in worse
 end
