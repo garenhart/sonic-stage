1e402a034f2147e9808de39c177b4d33462c1193
Garen Hart <garenhart@gmail.com>
2023-01-17 09:58:58 -0500

    Refactor OSC libs.

--- lib/lib-init.rb
+++ lib/lib-init.rb
@@ -4,6 +4,16 @@
 # author: Garen H.
 #######################
 
+define :parse_addr do |path|
+  e = get_event(path).to_s
+  v = e.split(",")[6]
+  if v != nil
+    return v[3..-2].split("/")
+  else
+    return ["error"]
+  end
+end
+
 # get the sample group name from a sample name (first substring before "_")
 define :sample_group do |s|
   s.split("_").first
@@ -24,56 +34,6 @@ define :reset_tonics do |cfg|
   cfg['tonics'] = []
   cfg['bass']['pattern'] = []
   cfg['chords']['pattern'] = []
-end
-
-define :init_keyboard do |tonic, mode|
-  scale_notes = scale tonic, mode
-  for note in 21..107
-    osc_ctrl "/keyboard_scale", note, (scale_notes.to_a.include? note) ? 1 : 0
-  end
-end
-
-define :init_drum do |d, gr_ctrl, inst_ctrl, cfg|
-  sample_gr = sample_group(cfg[d]['sample'])
-  osc_ctrl "/#{d}", cfg[d]['on'] ? 1 : 0
-  osc_ctrl "/#{d}_amp", cfg[d]['amp']
-  osc_ctrl gr_ctrl, sample_gr
-  populate_samples inst_ctrl + "_v", sample_gr.to_sym
-  sleep 0.125 # sleeping between populating and selecting seems to make it work 
-  osc_ctrl inst_ctrl, (cfg[d]['sample'])
-  16.times do |i|
-    osc_ctrl "/#{d}_beats/#{i}", cfg[d]['beats'][i] #should figure out how to populate beats without looping through array
-  end
-  # osc_ctrl "/#{d}_beats", cfg[d]['beats']
-  # set (d.to_sym), cfg[d]['beats'] #set the beats to for the drum
-end
-
-define :init_drums do |cfg|
-  osc_ctrl "/drums", 1
-  osc_ctrl "/dropdown_drum_tempo_factor", cfg['drum_tempo_factor']
-  init_drum "kick", "/kick_inst_groups", "/kick_inst", cfg
-  init_drum "snare", "/snare_inst_groups", "/snare_inst", cfg
-  init_drum "cymbal", "/cymbal_inst_groups", "/cymbal_inst", cfg
-end
-
-define :init_controls do |cfg|
-  osc_ctrl "/tempo", cfg['tempo']
-  osc_ctrl "/pattern_mode", cfg['pattern_mode']
-  osc_ctrl "/pattern", cfg['pattern']
-  osc_ctrl "/switch_loop", cfg['loop_mode']
-  osc_ctrl "/mode", cfg['mode']
-  osc_ctrl "/scale", cfg['scale']
-  osc_ctrl "/bass_inst", cfg['bass']['synth']
-  osc_ctrl "/bass_amp", cfg['bass']['amp']
-  osc_ctrl "/chord_type", cfg['chords']['type']
-  osc_ctrl "/chord_inst", cfg['chords']['synth']
-  osc_ctrl "/chord_amp", cfg['chords']['amp']
-  
-  osc_ctrl "/bass_points", cfg['tonics'].length
-  osc_ctrl "/chord_points", cfg['tonics'].length
-  init_keyboard(cfg['tonics'][0], cfg['scale'])
-
-  init_drums cfg
 end
 
   
--- lib/lib-osc.rb
+++ lib/lib-osc.rb
@@ -4,31 +4,14 @@
 # author: Garen H.
 #######################
 
-define :parse_addr do |path|
-    e = get_event(path).to_s
-    v = e.split(",")[6]
-    if v != nil
-      return v[3..-2].split("/")
-    else
-      return ["error"]
-    end
-  end
-
-# populates osc variable target with the list of SPi sample groups
-define :populate_sample_groups do |target|
-  return if target==nil
-  sg = sample_groups
-  sg_str = []
-  # convert to array of strings
-  for n in sg
-    sg_str.push n.to_s
-  end
-  osc_ctrl target, sg_str.to_s
+# directs osc message to open stage control
+define :osc_ctrl do |path, *args|
+  osc_send get(:ctrl_ip), get(:ctrl_port), path, *args
 end
 
 # populates osc variable target with the list of SPi sample names
 # for the specified sample group sg
-define :populate_samples do |target, sg|
+define :init_osc_samples do |target, sg|
   puts "pop", target, sg
   return if target==nil or sg==nil
   sn = sample_names(sg)
@@ -42,8 +25,65 @@ define :populate_samples do |target, sg|
   osc_ctrl target, sn_str
 end
 
-# directs osc message to open stage control
-define :osc_ctrl do |path, *args|
-  osc_send get(:ctrl_ip), get(:ctrl_port), path, *args
+define :init_osc_keyboard do |tonic, mode|
+  scale_notes = scale tonic, mode
+  for note in 21..107
+    osc_ctrl "/keyboard_scale", note, (scale_notes.to_a.include? note) ? 1 : 0
+  end
 end
+
+define :init_osc_drum do |d, gr_ctrl, inst_ctrl, cfg|
+  sample_gr = sample_group(cfg[d]['sample'])
+  osc_ctrl "/#{d}", cfg[d]['on'] ? 1 : 0
+  osc_ctrl "/#{d}_amp", cfg[d]['amp']
+  osc_ctrl gr_ctrl, sample_gr
+  init_osc_samples inst_ctrl + "_v", sample_gr.to_sym
+  sleep 0.125 # sleeping between populating and selecting seems to make it work 
+  osc_ctrl inst_ctrl, (cfg[d]['sample'])
+  16.times do |i|
+    osc_ctrl "/#{d}_beats/#{i}", cfg[d]['beats'][i] #should figure out how to populate beats without looping through array
+  end
+  # osc_ctrl "/#{d}_beats", cfg[d]['beats']
+  # set (d.to_sym), cfg[d]['beats'] #set the beats to for the drum
+end
+
+define :init_osc_drums do |cfg|
+  osc_ctrl "/drums", 1
+  osc_ctrl "/dropdown_drum_tempo_factor", cfg['drum_tempo_factor']
+  init_osc_drum "kick", "/kick_inst_groups", "/kick_inst", cfg
+  init_osc_drum "snare", "/snare_inst_groups", "/snare_inst", cfg
+  init_osc_drum "cymbal", "/cymbal_inst_groups", "/cymbal_inst", cfg
+end
+
+define :init_osc_controls do |cfg|
+  osc_ctrl "/tempo", cfg['tempo']
+  osc_ctrl "/pattern_mode", cfg['pattern_mode']
+  osc_ctrl "/pattern", cfg['pattern']
+  osc_ctrl "/switch_loop", cfg['loop_mode']
+  osc_ctrl "/mode", cfg['mode']
+  osc_ctrl "/scale", cfg['scale']
+  osc_ctrl "/bass_inst", cfg['bass']['synth']
+  osc_ctrl "/bass_amp", cfg['bass']['amp']
+  osc_ctrl "/chord_type", cfg['chords']['type']
+  osc_ctrl "/chord_inst", cfg['chords']['synth']
+  osc_ctrl "/chord_amp", cfg['chords']['amp']
+  
+  osc_ctrl "/bass_points", cfg['tonics'].length
+  osc_ctrl "/chord_points", cfg['tonics'].length
+  init_osc_keyboard(cfg['tonics'][0], cfg['scale'])
+
+  init_osc_drums cfg
+end
+
+# populates osc variable target with the list of SPi sample groups
+# define :osc_populate_sample_groups do |target|
+#   return if target==nil
+#   sg = sample_groups
+#   sg_str = []
+#   # convert to array of strings
+#   for n in sg
+#     sg_str.push n.to_s
+#   end
+#   osc_ctrl target, sg_str.to_s
+# end
 
