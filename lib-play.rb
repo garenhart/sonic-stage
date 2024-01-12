0c2360d439cf7b72863f9b65c70fc19ec7287359
Garen Hart <garenhart@gmail.com>
2024-01-12 16:50:43 -0500

    Pitch shift implementation for drum samples.

--- lib-play.rb
+++ lib-play.rb
@@ -41,16 +41,17 @@ define :play_drum do |drum, cfg|
       amp = rt_drums[drum]['amp']
       start = rt_drums[drum]['reverse'] ? rt_drums[drum]['range'][1] : rt_drums[drum]['range'][0]
       finish = rt_drums[drum]['reverse'] ? rt_drums[drum]['range'][0] : rt_drums[drum]['range'][1]
+      pitch_shift = rt_drums[drum]['pitch_shift']
 
       if rt_drums[drum]['on'] 
         if (beats[i] == "1")
           if (rt_drums[drum]['random'])
-            sample rt_drums[drum]['sample'], amp: amp, onset: pick
+            sample rt_drums[drum]['sample'], amp: amp, onset: pick, pitch: pitch_shift
           else  
             if (start == finish)
-              sample rt_drums[drum]['sample'], amp: amp, onset: 0
+              sample rt_drums[drum]['sample'], amp: amp, onset: 0, pitch: pitch_shift
             else
-              sample rt_drums[drum]['sample'], amp: amp, start: start, finish: finish 
+              sample rt_drums[drum]['sample'], amp: amp, start: start, finish: finish, pitch: pitch_shift 
             end
           end  
           animate_drum drum, amp, 1, 1
