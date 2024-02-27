35d1453304088931a1351df36f194fd2784bb5de
Garen Hart <garenhart@gmail.com>
2024-02-27 10:03:45 -0500

    Add support for favorite instruments (synths) - solo Fixes #22 (sonic-stage)

--- lib-fav.rb
+++ lib-fav.rb
@@ -29,3 +29,30 @@ define :update_fav_drums do |cfg, d, add|
       remove_fav_drum cfg, d
     end    
 end
+
+define :solo_fav? do |cfg, v|
+    cfg['fav'].include?(v.to_s) if cfg['fav']
+end
+
+define :add_fav do |cfg|
+    # add 'fav' key to the samples hash if it doesn't exist
+    cfg['fav'] = [] unless cfg['fav']
+
+    # add current selection to the favorites list
+    cfg['fav'] << cfg['solo_inst'].to_s unless solo_fav? cfg, cfg['solo_inst']
+    cfg['fav'].uniq!
+end
+
+define :remove_fav do |cfg|
+    # remove current selection from the favorites list if it exists
+    cfg['fav'].delete(cfg['solo_inst'].to_s)
+end
+
+define :update_fav do |cfg, add|
+    # update the favorites list
+    if add == 1.0
+      add_fav cfg
+    else
+      remove_fav cfg
+    end    
+end
