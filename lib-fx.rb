5b1f01870b2bb1cb23705d34dc651f7e9299312f
Garen Hart <garenhart@gmail.com>
2024-04-09 14:26:29 -0400

    Refactor: move fx related functions to lib-fx.rb

--- lib-fx.rb
+++ lib-fx.rb
@@ -34,4 +34,60 @@ define :init_fx_component do |cfg, inst, n, nn, v|
     cfg[inst]['fx'] = [["", 0, 0]]
     cfg[inst]['fx'][n][nn] = v
   end  
-end  
+end
+
+def fx_chain_rand(cfg_fx)
+  if cfg_fx && cfg_fx.length > 0
+    chain = lambda do
+      [
+        {with_fx: :echo, phase: rrand(0,0.5)},
+        {with_fx: :whammy, grainsize: rrand(0,2.0)},
+        {with_fx: :flanger, feedback: rrand(0,0.5)},
+        {with_fx: :tremolo, phase: rrand(0,0.5)},
+        {with_fx: :bitcrusher, bits: rand_i(3..5)}
+      ].pick(3) # Use all or pick some randomly
+    end
+  else
+    chain = lambda do
+      [{with_fx: :none}]
+    end
+  end
+  chain.call
+end
+
+def fx_chain(cfg_fx)
+  if cfg_fx && cfg_fx.length > 0
+    chain = lambda do
+      cfg_fx.map do |fx|
+        puts "FX", fx
+        options = { with_fx: fx[0].to_sym }
+        options[fx_option_name(fx[0], 1).to_sym] = fx[1].is_a?(Array) ? rrand(fx[1][0], fx[1][1]) : fx[1]
+        options[fx_option_name(fx[0], 2).to_sym] = fx[2].is_a?(Array) ? rrand(fx[2][0], fx[2][1]) : fx[2]
+        options
+      end
+    end
+  else
+    chain = lambda do
+      [{ with_fx: :none }]
+    end
+  end
+  puts "FX CHAIN = ", chain.call
+  chain.call
+end
+
+# This method and the idea of using it is borrowed from 
+# @amiika here: https://in-thread.sonic-pi.net/t/snake-jazz-also-is-there-an-fx-stack/5932/3
+def with_effects(x, &block)
+  x = x.dup
+  if x.length>0 then
+    n = x.shift
+    if n[:with_fx] then
+      with_fx n[:with_fx], n do
+        with_effects(x, &block)
+      end
+    end
+  else
+    yield
+  end
+end
+
