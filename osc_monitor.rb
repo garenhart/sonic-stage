1dac85b3ea55036df55feca4c91f978aef1c57ba
Garen Hart <garenhart@gmail.com>
2024-04-15 16:46:15 -0400

    Cleanup

--- osc_monitor.rb
+++ osc_monitor.rb
@@ -130,256 +130,253 @@ live_loop :osc_monitor do
   n = sync addr
   token = parse_addr addr
  
-  # if token[1] starts with "solo" or "chord" or "bass" or "kick" or "snare" or "cymbal"
-  # then call inst_mon(token[1], inst, cfg) where inst is the first element of token[1]
   t = token[1].split("_")[0]
-  if t == "kick"  || t == "snare" || t == "cymbal" #t == "solo" || t == "chord" || t == "bass"
+  if t == "kick"  || t == "snare" || t == "cymbal"
     puts "TOKEN========", t
     drum_mon token, t, n, cfg
   else
+    case token[1]
+    when "open"
+      cfgFile = n[0]
+      # deserialize JSON file into cfg hash
+      data = readJSON(cfgFile)
 
-  case token[1]
-  when "open"
-    cfgFile = n[0]
-    # deserialize JSON file into cfg hash
-    data = readJSON(cfgFile)
+      ca = get(:chord_auto)
+      ba = get(:bass_auto)
+      da = get(:drums_auto)
 
-    ca = get(:chord_auto)
-    ba = get(:bass_auto)
-    da = get(:drums_auto)
+      # accept data only if all _auto states are true
+      # or new "tempo" is the same as current tempo
+      if (ca && ba && da) || (data['tempo'] == cfg['tempo'])
+        cfg = data
+        # init osc controls twice to avoid blank instruments
+        init_osc_controls cfg
+        init_osc_controls cfg
 
-    # accept data only if all _auto states are true
-    # or new "tempo" is the same as current tempo
-    if (ca && ba && da) || (data['tempo'] == cfg['tempo'])
-      cfg = data
-      # init osc controls twice to avoid blank instruments
-      init_osc_controls cfg
-      init_osc_controls cfg
+        init_time_state_chord cfg if get(:chord_auto)
+        init_time_state_bass cfg if get(:bass_auto)
+        init_time_state_drums cfg if get(:drums_auto)
+      else
+        osc_ctrl "/NOTIFY", "triangle-exclamation", "Tempo mismatch! Cannot load " + cfgFile
+      end
+      
+    when "save"
+      # serialize cfg hash into JSON file
+      new_name = write_unique_JSON(cfgFile, cfg)
+      osc_ctrl "/NOTIFY", "folder-plus", new_name + " saved"
+      
+    when "tempo"
+      cfg['tempo'] = n[0].to_i
+      
+    when "pattern"
+      cfg['pattern'] = n[0].to_i
+      
+    when "pattern_mode"
+      cfg['pattern_mode'] = n[0].to_i
+      if n[0] == 1.0
+        reset_tonics cfg
+      end
+      
+    when "switch_loop"
+      cfg['loop_mode'] = n[0].to_i
+      cfg['pattern_mode'] = 0 if n[0].to_i > 0
 
+    when "solo_inst"
+      cfg['solo']['inst'] = n[0].to_sym
+      osc_ctrl "/solo_fav", solo_fav?(cfg, cfg['solo']['inst']) ? 1 : 0
+
+    when "solo_on"
+      cfg['solo']['on'] = n[0] == 1.0
+
+    when "solo_fav"
+      update_fav cfg, n[0]
+
+    when "solo_fav_all"
+      cfg['solo']['fav_all'] = n[0] == 1.0
+
+    when "solo_fx1_fx"
+      init_fx_component cfg, "solo", 0, 0, n[0]
+      update_osc_fx_option_names "solo", n[0], 1
+
+    when "solo_fx1_opt1_value"
+      init_fx_component cfg, "solo", 0, 1, n
+
+    when "solo_fx1_opt2_value"
+      init_fx_component cfg, "solo", 0, 2, n
+
+    when "solo_fx2_fx"
+      init_fx_component cfg, "solo", 1, 0, n[0]
+      update_osc_fx_option_names "solo", n[0], 2
+
+    when "solo_fx2_opt1_value"
+      init_fx_component cfg, "solo", 1, 1, n
+
+    when "solo_fx2_opt2_value"
+      init_fx_component cfg, "solo", 1, 2, n
+
+  # chord section ==================================    
+    when "chord_pt_count"
+      update_chord_count cfg, n[0].to_i
+    
+    when "chord_dup_data"
+      clone_chord_pattern cfg
+
+    when "chord_tempo_factor" # update Time State
+      cfg['chord']['tempo_factor'] = n[0].to_i
       init_time_state_chord cfg if get(:chord_auto)
+
+    when "chord_update" # update Time State
+      init_time_state_chord cfg if n[0] == 0.0
+      
+    when "chord_auto"
+      set :chord_auto, n[0].to_i == 1 ? true : false
+
+    when "chord_inst"
+      init_chord_component(cfg, "synth", n[0].to_sym)
+      
+    when "chord_type"
+      init_chord_component(cfg, "type", n[0].to_i)    
+
+    when "chord_line_updated"
+      # add elements with even indices (0, 2, 4...) of array n to bass pattern
+      # (we only need x coordinates), and convert to integer
+      init_chord_component(cfg, "pattern", (n.select.with_index { |_, i| i.even? }).map { |x| x.to_i })
+
+    when "chord_on"
+      init_chord_component(cfg, "on", n[0]==1.0)
+
+    when "chord_fav"
+      update_fav_chord cfg, n[0]
+
+    when "chord_fav_all"
+      cfg['chord']['fav_all'] = n[0] == 1.0
+
+      
+
+    when "chord_fx1_fx"
+      init_fx_component cfg, "chord", 0, 0, n[0]
+      update_osc_fx_option_names "chord", n[0], 1
+
+    when "chord_fx1_opt1_value"
+      init_fx_component cfg, "chord", 0, 1, n
+
+    when "chord_fx1_opt2_value"
+      init_fx_component cfg, "chord", 0, 2, n
+
+    when "chord_fx2_fx"
+      init_fx_component cfg, "chord", 1, 0, n[0]
+      update_osc_fx_option_names "chord", n[0], 2
+
+    when "chord_fx2_opt1_value"
+      init_fx_component cfg, "chord", 1, 1, n
+
+    when "chord_fx2_opt2_value"
+      init_fx_component cfg, "chord", 1, 2, n
+
+
+    when "chord_amp"
+      init_chord_component(cfg, "amp", n[0])
+    
+    when "chord_delete"
+      # cast n array to array of integers
+      n = n.map { |x| x.to_i }    
+      delete_chord_pattern cfg, n
+
+    # bass section ===================================    
+    when "bass_pt_count"
+      update_bass_count cfg, n[0].to_i
+
+    when "bass_dup_data"
+      clone_bass_pattern cfg
+
+    when "bass_tempo_factor" # update Time State
+      cfg['bass']['tempo_factor'] = n[0].to_i
       init_time_state_bass cfg if get(:bass_auto)
+
+    when "bass_update" # update Time State
+      init_time_state_bass cfg if n[0] == 0.0
+      
+    when "bass_auto"
+      set :bass_auto, n[0].to_i == 1 ? true : false
+
+    when "bass_inst"
+      init_bass_component(cfg, 'synth', n[0].to_sym)
+      
+    when "bass_line_updated"
+      # add elements with even indices (0, 2, 4...) of array n to bass pattern
+      # (we only need x coordinates), and convert to integer
+      init_bass_component(cfg, 'pattern', (n.select.with_index { |_, i| i.even? }).map { |x| x.to_i })
+
+    when "bass_on"
+      init_bass_component(cfg, 'on', n[0]==1.0)
+
+    when "bass_fav"
+      update_fav_bass cfg, n[0]
+
+    when "bass_fav_all"
+      cfg['bass']['fav_all'] = n[0] == 1.0
+
+    when "bass_fx1_fx"
+      init_fx_component cfg, "bass", 0, 0, n[0]
+      update_osc_fx_option_names "bass", n[0], 1
+
+    when "bass_fx1_opt1_value"
+      init_fx_component cfg, "bass", 0, 1, n
+
+    when "bass_fx1_opt2_value"
+      init_fx_component cfg, "bass", 0, 2, n
+
+    when "bass_fx2_fx"
+      init_fx_component cfg, "bass", 1, 0, n[0]
+      update_osc_fx_option_names "bass", n[0], 2
+
+    when "bass_fx2_opt1_value"
+      init_fx_component cfg, "bass", 1, 1, n
+
+    when "bass_fx2_opt2_value"
+      init_fx_component cfg, "bass", 1, 2, n
+
+
+    when "bass_amp"
+      init_bass_component(cfg, 'amp', n[0])
+
+    when "bass_delete"
+      # cast n array to array of integers
+      n = n.map { |x| x.to_i }    
+      delete_bass_pattern cfg, n
+    
+    # drum section ==================================
+    when "beat_pt_count"
+      update_drum_beats cfg, n[0].to_i
+
+    when "drum_dup_data"
+      clone_drums_beats cfg
+      
+    when "drum_tempo_factor" # update Time State
+      cfg['drums']['tempo_factor'] = n[0].to_i
       init_time_state_drums cfg if get(:drums_auto)
-    else
-      osc_ctrl "/NOTIFY", "triangle-exclamation", "Tempo mismatch! Cannot load " + cfgFile
+
+    when "drums_update" # update Time State
+      init_time_state_drums cfg if n[0] == 0.0
+      
+    when "drums_auto"
+      set :drums_auto, n[0].to_i == 1 ? true : false
+
+  # save mode and scale
+    when "mode"
+      cfg['mode'] = n[0].to_i
+    when "scale"
+      cfg['scale'] = n[0].to_sym
+      puts "SSSSSSSSSSSSSSSSS", cfg['scale']
+      update_scale_match cfg
+
+    # recording
+    when "bass_rec"
+      set :bass_rec, n[0].to_i == 1 ? true : false
+
+    when "chord_rec"
+      set :chord_rec, n[0].to_i == 1 ? true : false    
     end
-    
-  when "save"
-    # serialize cfg hash into JSON file
-    new_name = write_unique_JSON(cfgFile, cfg)
-    osc_ctrl "/NOTIFY", "folder-plus", new_name + " saved"
-    
-  when "tempo"
-    cfg['tempo'] = n[0].to_i
-    
-  when "pattern"
-    cfg['pattern'] = n[0].to_i
-    
-  when "pattern_mode"
-    cfg['pattern_mode'] = n[0].to_i
-    if n[0] == 1.0
-      reset_tonics cfg
-    end
-    
-  when "switch_loop"
-    cfg['loop_mode'] = n[0].to_i
-    cfg['pattern_mode'] = 0 if n[0].to_i > 0
-
-  when "solo_inst"
-    cfg['solo']['inst'] = n[0].to_sym
-    osc_ctrl "/solo_fav", solo_fav?(cfg, cfg['solo']['inst']) ? 1 : 0
-
-  when "solo_on"
-    cfg['solo']['on'] = n[0] == 1.0
-
-  when "solo_fav"
-    update_fav cfg, n[0]
-
-  when "solo_fav_all"
-    cfg['solo']['fav_all'] = n[0] == 1.0
-
-  when "solo_fx1_fx"
-    init_fx_component cfg, "solo", 0, 0, n[0]
-    update_osc_fx_option_names "solo", n[0], 1
-
-  when "solo_fx1_opt1_value"
-    init_fx_component cfg, "solo", 0, 1, n
-
-  when "solo_fx1_opt2_value"
-    init_fx_component cfg, "solo", 0, 2, n
-
-  when "solo_fx2_fx"
-    init_fx_component cfg, "solo", 1, 0, n[0]
-    update_osc_fx_option_names "solo", n[0], 2
-
-  when "solo_fx2_opt1_value"
-    init_fx_component cfg, "solo", 1, 1, n
-
-  when "solo_fx2_opt2_value"
-    init_fx_component cfg, "solo", 1, 2, n
-
-# chord section ==================================    
-  when "chord_pt_count"
-    update_chord_count cfg, n[0].to_i
-  
-  when "chord_dup_data"
-    clone_chord_pattern cfg
-
-  when "chord_tempo_factor" # update Time State
-    cfg['chord']['tempo_factor'] = n[0].to_i
-    init_time_state_chord cfg if get(:chord_auto)
-
-  when "chord_update" # update Time State
-    init_time_state_chord cfg if n[0] == 0.0
-    
-  when "chord_auto"
-    set :chord_auto, n[0].to_i == 1 ? true : false
-
-  when "chord_inst"
-    init_chord_component(cfg, "synth", n[0].to_sym)
-    
-  when "chord_type"
-    init_chord_component(cfg, "type", n[0].to_i)    
-
-  when "chord_line_updated"
-    # add elements with even indices (0, 2, 4...) of array n to bass pattern
-    # (we only need x coordinates), and convert to integer
-    init_chord_component(cfg, "pattern", (n.select.with_index { |_, i| i.even? }).map { |x| x.to_i })
-
-  when "chord_on"
-    init_chord_component(cfg, "on", n[0]==1.0)
-
-  when "chord_fav"
-    update_fav_chord cfg, n[0]
-
-  when "chord_fav_all"
-    cfg['chord']['fav_all'] = n[0] == 1.0
-
-    
-
-  when "chord_fx1_fx"
-    init_fx_component cfg, "chord", 0, 0, n[0]
-    update_osc_fx_option_names "chord", n[0], 1
-
-  when "chord_fx1_opt1_value"
-    init_fx_component cfg, "chord", 0, 1, n
-
-  when "chord_fx1_opt2_value"
-    init_fx_component cfg, "chord", 0, 2, n
-
-  when "chord_fx2_fx"
-    init_fx_component cfg, "chord", 1, 0, n[0]
-    update_osc_fx_option_names "chord", n[0], 2
-
-  when "chord_fx2_opt1_value"
-    init_fx_component cfg, "chord", 1, 1, n
-
-  when "chord_fx2_opt2_value"
-    init_fx_component cfg, "chord", 1, 2, n
-
-
-  when "chord_amp"
-    init_chord_component(cfg, "amp", n[0])
-  
-  when "chord_delete"
-    # cast n array to array of integers
-    n = n.map { |x| x.to_i }    
-    delete_chord_pattern cfg, n
-
-  # bass section ===================================    
-  when "bass_pt_count"
-    update_bass_count cfg, n[0].to_i
-
-  when "bass_dup_data"
-    clone_bass_pattern cfg
-
-  when "bass_tempo_factor" # update Time State
-    cfg['bass']['tempo_factor'] = n[0].to_i
-    init_time_state_bass cfg if get(:bass_auto)
-
-  when "bass_update" # update Time State
-    init_time_state_bass cfg if n[0] == 0.0
-    
-  when "bass_auto"
-    set :bass_auto, n[0].to_i == 1 ? true : false
-
-  when "bass_inst"
-    init_bass_component(cfg, 'synth', n[0].to_sym)
-    
-  when "bass_line_updated"
-    # add elements with even indices (0, 2, 4...) of array n to bass pattern
-    # (we only need x coordinates), and convert to integer
-    init_bass_component(cfg, 'pattern', (n.select.with_index { |_, i| i.even? }).map { |x| x.to_i })
-
-  when "bass_on"
-    init_bass_component(cfg, 'on', n[0]==1.0)
-
-  when "bass_fav"
-    update_fav_bass cfg, n[0]
-
-  when "bass_fav_all"
-    cfg['bass']['fav_all'] = n[0] == 1.0
-
-  when "bass_fx1_fx"
-    init_fx_component cfg, "bass", 0, 0, n[0]
-    update_osc_fx_option_names "bass", n[0], 1
-
-  when "bass_fx1_opt1_value"
-    init_fx_component cfg, "bass", 0, 1, n
-
-  when "bass_fx1_opt2_value"
-    init_fx_component cfg, "bass", 0, 2, n
-
-  when "bass_fx2_fx"
-    init_fx_component cfg, "bass", 1, 0, n[0]
-    update_osc_fx_option_names "bass", n[0], 2
-
-  when "bass_fx2_opt1_value"
-    init_fx_component cfg, "bass", 1, 1, n
-
-  when "bass_fx2_opt2_value"
-    init_fx_component cfg, "bass", 1, 2, n
-
-
-  when "bass_amp"
-    init_bass_component(cfg, 'amp', n[0])
-
-  when "bass_delete"
-    # cast n array to array of integers
-    n = n.map { |x| x.to_i }    
-    delete_bass_pattern cfg, n
-  
-  # drum section ==================================
-  when "beat_pt_count"
-    update_drum_beats cfg, n[0].to_i
-
-  when "drum_dup_data"
-    clone_drums_beats cfg
-    
-  when "drum_tempo_factor" # update Time State
-    cfg['drums']['tempo_factor'] = n[0].to_i
-    init_time_state_drums cfg if get(:drums_auto)
-
-  when "drums_update" # update Time State
-    init_time_state_drums cfg if n[0] == 0.0
-    
-  when "drums_auto"
-    set :drums_auto, n[0].to_i == 1 ? true : false
-
- # save mode and scale
-  when "mode"
-    cfg['mode'] = n[0].to_i
-  when "scale"
-    cfg['scale'] = n[0].to_sym
-    puts "SSSSSSSSSSSSSSSSS", cfg['scale']
-    update_scale_match cfg
-
-  # recording
-  when "bass_rec"
-    set :bass_rec, n[0].to_i == 1 ? true : false
-
-  when "chord_rec"
-    set :chord_rec, n[0].to_i == 1 ? true : false    
   end
-end
 end
 # END OSC MESSAGE MONITORING LOOP
