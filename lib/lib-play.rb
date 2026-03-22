6adf4a543f4fcfddf662595327eabd318db01bc2
Garen Hart <garenhart@gmail.com>
2026-03-22 14:33:45 -0400

    Refector midi loops

--- lib/lib-play.rb
+++ lib/lib-play.rb
@@ -250,24 +250,24 @@ end
 define :play_midi_solo do |cfg, addr_data, note, vel|
   with_effects fx_chain(cfg['solo']['fx']) do
     play_synth_note cfg['solo']['inst'].to_sym, note, vel/127.0, cfg['solo']['adsr']
-    animate_keyboard "solo", note, vel/127.0 if cfg['solo']['animate']
   end
+  animate_keyboard "solo", note, vel/127.0 if cfg['solo']['animate']
 end
 
 define :play_midi_bass do |bass_rec, cfg, addr_data, note, vel, next_beat|
   with_effects fx_chain(cfg['bass']['fx']) do
     play_synth_note cfg['bass']['synth'].to_sym, note, vel/127.0, cfg['bass']['adsr']
-    animate_keyboard "bass", note, vel/127.0 if cfg['bass']['animate']
-    add_tonic_bass cfg, note, next_beat > cfg['bass']['count'] ? 1 : next_beat
   end
+  animate_keyboard "bass", note, vel/127.0 if cfg['bass']['animate']
+  add_tonic_bass cfg, note, next_beat > cfg['bass']['count'] ? 1 : next_beat
 end
 
 define :play_midi_chord do |chord_rec, cfg, addr_data, note, vel, next_beat|
   with_effects fx_chain(cfg['chord']['fx']) do
     play_synth_note cfg['chord']['synth'].to_sym, note, vel/127.0, cfg['chord']['adsr']
-    animate_keyboard "chord", note, vel/127.0 if cfg['chord']['animate']
-    add_tonic_chord cfg, note, next_beat > cfg['chord']['count'] ? 1 : next_beat
   end
+  animate_keyboard "chord", note, vel/127.0 if cfg['chord']['animate']
+  add_tonic_chord cfg, note, next_beat > cfg['chord']['count'] ? 1 : next_beat
 end
 
 
